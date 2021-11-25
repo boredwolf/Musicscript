6b394f2d5c15ec19ab83b8b410a5aad00c18c251
boredwolf <felix.jouvhomme@gmail.com>
2021-11-25 11:50:52 +0100

    base

---
+++ musiscript/musiscript/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="min-h-screen">
+      
+      <h2 className="text-pink-700 text-center text-3xl bg-black-300" >BOXIFY</h2>
+  
+    </div>
+  )
+}
+
+export default App
