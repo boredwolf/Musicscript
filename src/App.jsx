37a7f8fbd45f469257de6789d1cbe08d7b3c957d
Félix Jouvhomme <80586378+boredwolf@users.noreply.github.com>
2021-11-25 22:01:19 +0100

    Homev (#4)
    * hahahahahahahahahahaha
    
    * hahahahahahahahahahaha
    
    * hahahaha
    
    * hahahah
    
    * hahahah
    
    * hahahah
    
    * hahahah
    
    * hahahah

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import { useState } from 'react'
 import {
   BrowserRouter as Router,
@@ -15,25 +16,23 @@ import Choice from './Components/Choice';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <Router>
     <div className="min-h-screen">
       <Navbar />
       <Routes>
-        <Route path="/home"></Route>
+        <Route path="/" element={<Home/>}/>
         <Route path="/connexion" element={<Connexion/>}> </Route>
         <Route path="/formules" element={<Formules/>}> </Route>
         <Route path="/choice" element={<Choice/>}> </Route>
-        <Route exact path="/">
               
-        </Route>
         <Route path="/location" element={<Location/>} />
       </Routes>
     </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
