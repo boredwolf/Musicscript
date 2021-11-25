634ad4ac5569582a174167b3e6d5fe89bae18b56
boredwolf <felix.jouvhomme@gmail.com>
2021-11-25 21:51:26 +0100

    hahahah

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
@@ -10,7 +9,6 @@ import Location from "./Components/Location";
 import Home from "./Components/Home";
 
 import Formules from "./Components/Formules";
-=======
 import { useState } from 'react'
 import {
   BrowserRouter as Router,
@@ -26,40 +24,22 @@ import Home from './Components/Home';
 import Formules from './Components/Formules';
 import Choice from './Components/Choice';
 
->>>>>>> c0bb187ad418e1302c5d3c11933a59b8e277bdaa
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <Router>
-<<<<<<< HEAD
-      <div className="min-h-screen">
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/connexion" element={<Connexion />}/>
-          <Route path="/formules" element={<Formules />}>
-          </Route>
-          <Route exact path="/"></Route>
-          <Route path="/location" element={<Location />} />
-        </Routes>
-      </div>
-=======
     <div className="min-h-screen">
       <Navbar />
       <Routes>
-        <Route path="/home"></Route>
+        <Route path="/" element={<Home/>}/>
         <Route path="/connexion" element={<Connexion/>}> </Route>
         <Route path="/formules" element={<Formules/>}> </Route>
         <Route path="/choice" element={<Choice/>}> </Route>
-        <Route exact path="/">
-              
-        </Route>
         <Route path="/location" element={<Location/>} />
       </Routes>
     </div>
->>>>>>> c0bb187ad418e1302c5d3c11933a59b8e277bdaa
     </Router>
   );
 }
