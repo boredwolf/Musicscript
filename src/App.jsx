7af72016a7c8430aea3428a0dd70d14db616ef9a
boredwolf <felix.jouvhomme@gmail.com>
2021-11-25 19:01:05 +0100

    hahahahahahahahahahaha

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,38 @@
-import { useState } from 'react'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import './App.css'
-import Navbar from './Components/Navbar'
-import Connexion from './Components/Connexion'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Connexion from "./Components/Connexion";
 
-import Location from './Components/Location'
-<<<<<<< HEAD
-import Home from './Components/Home';
-=======
-import Formules from './Components/Formules';
->>>>>>> develop
+import Location from "./Components/Location";
+
+import Home from "./Components/Home";
+
+import Formules from "./Components/Formules";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <Router>
-    <div className="min-h-screen">
-      <Navbar />
-      <Routes>
-<<<<<<< HEAD
-        <Route exact path="/" element={<Home/>} />
-        
-=======
-        <Route path="/home"></Route>
-        <Route path="/connexion" element={<Connexion/>}> </Route>
-        <Route path="/formules" element={<Formules/>}> </Route>
-        <Route exact path="/">
-              
-        </Route>
->>>>>>> develop
-        <Route path="/location" element={<Location/>} />
-      </Routes>
-    </div>
+      <div className="min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+
+          <Route path="/home"></Route>
+          <Route path="/connexion" element={<Connexion />}>
+            {" "}
+          </Route>
+          <Route path="/formules" element={<Formules />}>
+            {" "}
+          </Route>
+          <Route exact path="/"></Route>
+          <Route path="/location" element={<Location />} />
+        </Routes>
+      </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
