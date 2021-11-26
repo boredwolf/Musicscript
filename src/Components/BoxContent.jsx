b43d514a4153f9f6d9b9f00b7d5d45de287df4fb
Kevin <kevin.devilliers@gmail.com>
2021-11-26 04:20:50 +0100

    final

--- src/Components/BoxContent.jsx
+++ src/Components/BoxContent.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function BoxContent() {
     return (
-        <div className="background-main w-screen h-screen">
-        <div className="flex flex-col items-center rounded-lg pt-4 text-gray-50 border-gray-50 border-4">
+        <div className="background-main w-screen h-screen flex flex-col ">
+        <div className="flex flex-col items-center rounded-lg pt-4 text-gray-50 border-gray-50 border-4 w-3/5 m-auto h-5/6">
             <h1 className="text-center w-5/6 mx-auto px-4 text-5xl font-extrabold pb-4">
   Content of the monthly box
 </h1>
@@ -11,7 +11,7 @@ function BoxContent() {
 The aim of this box is for you to enjoy and discover the musical scene in the selected area. From small independant artists to world wide stars, Boxify has been specifically design for those who love live music.
 The artits are finely selected by our experts to make sure that you'll will be pleased with the event.
 </p>
-            <img className="rounded-lg w-5/6 mb-4" src="https://unblast.com/wp-content/uploads/2019/10/Event-Ticket-Mockup-1.jpg" alt="ticket"/>
+            <img className="rounded-lg h-1/2 mb-4" src="https://unblast.com/wp-content/uploads/2019/10/Event-Ticket-Mockup-1.jpg" alt="ticket"/>
             
         </div>
         </div>
--- src/Components/Choice.jsx
+++ src/Components/Choice.jsx
@@ -42,58 +42,71 @@ function Choice() {
     setComponentRap(!componentRap);
   };
 
-
   return (
     <div className="h-screen pt-10 background-main">
-        <h1 className="text-center w-5/6 mx-auto px-4 text-5xl font-extrabold text-gray-900">
-  Select the kind of music you like
-</h1>
-<hr className="mt-4 w-1/4 mx-auto"></hr>
-      <div className="flex justify-between w-4/5 m-auto mt-10">
+      <h1 className="text-center w-5/6 mx-auto px-4 text-5xl font-extrabold text-gray-50">
+        Select the kind of music you like
+      </h1>
+      <hr className="mt-4 w-1/4 mx-auto"></hr>
+      <div className="flex justify-between w-4/5 m-auto mt-10 ">
         <button
-          className="rounded-lg text-xl px-4 py-2 bg-gray-300 hover:bg-purple-400 transition duration-700 ease-in-out transform hover:scale-125"
+          className="m-2 px-4 py-2 border-solid border-2 border-white text-white text-2xl rounded-full hover:bg-white hover:text-purple-700 transition duration-500 ease-in-out transform hover:scale-125"
           onClick={showRock}
         >
           Rock
         </button>
-        <button className="rounded-lg text-xl px-4 py-2 bg-gray-300 hover:bg-purple-400  transition duration-700 ease-in-out transform hover:scale-110 "
-        onClick={showPop}>
+        <button
+          className="m-2 px-4 py-2 border-solid border-2 border-white text-white text-2xl rounded-full hover:bg-white hover:text-purple-700 transition duration-500 ease-in-out transform hover:scale-125"
+          onClick={showPop}
+        >
           Pop
         </button>
-        <button className="rounded-lg text-xl px-4 py-2 bg-gray-300 hover:bg-purple-400  transition duration-700 ease-in-out transform hover:scale-110 "
-        onClick={showJazz}>
+        <button
+          className="m-2 px-4 py-2 border-solid border-2 border-white text-white text-2xl rounded-full hover:bg-white hover:text-purple-700 transition duration-500 ease-in-out transform hover:scale-125"
+          onClick={showJazz}
+        >
           Jazz
         </button>
-        <button className="rounded-lg text-xl px-4 py-2 bg-gray-300 hover:bg-purple-400  transition duration-700 ease-in-out transform hover:scale-110 "
-        onClick={showElectro}>
+        <button
+          className="m-2 px-4 py-2 border-solid border-2 border-white text-white text-2xl rounded-full hover:bg-white hover:text-purple-700 transition duration-500 ease-in-out transform hover:scale-125"
+          onClick={showElectro}
+        >
           Electro
         </button>
-        <button className="rounded-lg text-xl px-4 py-2 bg-gray-300 hover:bg-purple-400  transition duration-700 ease-in-out transform hover:scale-110 "
-        onClick={showMetal}>
+        <button
+          className="m-2 px-4 py-2 border-solid border-2 border-white text-white text-2xl rounded-full hover:bg-white hover:text-purple-700 transition duration-500 ease-in-out transform hover:scale-125"
+          onClick={showMetal}
+        >
           Metal
         </button>
-        <button className="rounded-lg text-xl px-4 py-2 bg-gray-300 hover:bg-purple-400  transition duration-700 ease-in-out transform hover:scale-110 "
-        onClick={showHiphop}>
+        <button
+          className="m-2 px-4 py-2 border-solid border-2 border-white text-white text-2xl rounded-full hover:bg-white hover:text-purple-700 transition duration-500 ease-in-out transform hover:scale-125"
+          onClick={showHiphop}
+        >
           Hip-Hop
         </button>
-        <button className="rounded-lg text-xl px-4 py-2 bg-gray-300 hover:bg-purple-400  transition duration-700 ease-in-out transform hover:scale-110 "
-        onClick={showReggae}>
+        <button
+          className="m-2 px-4 py-2 border-solid border-2 border-white text-white text-2xl rounded-full hover:bg-white hover:text-purple-700 transition duration-500 ease-in-out transform hover:scale-125"
+          onClick={showReggae}
+        >
           Reggae
         </button>
-        <button className="rounded-lg text-xl px-4 py-2 bg-gray-300 hover:bg-purple-400  transition duration-700 ease-in-out transform hover:scale-110 "
-        onClick={showRap}>
+        <button
+          className="m-2 px-4 py-2 border-solid border-2 border-white text-white text-2xl rounded-full hover:bg-white hover:text-purple-700 transition duration-500 ease-in-out transform hover:scale-125"
+          onClick={showRap}
+        >
           Rap
         </button>
       </div>
       <div className="flex w-screen justify-center">
-      <div> {componentRock ? <Rock /> : null}</div>
-      <div> {componentPop ? <Pop /> : null}</div>
-      <div> {componentJazz ? <Jazz /> : null}</div>
-      <div> {componentElectro ? <Electro /> : null}</div>
-      <div> {componentMetal ? <Metal /> : null}</div>
-      <div> {componentHiphop ? <Hiphop /> : null}</div>
-      <div> {componentReggae ? <Reggae /> : null}</div>
-      <div> {componentRap ? <Rap /> : null}</div>
+        <div> {componentRock ? <Rock /> : null}</div>
+        <div> {componentPop ? <Pop /> : null}</div>
+        <div> {componentJazz ? <Jazz /> : null}</div>
+        <div> {componentElectro ? <Electro /> : null}</div>
+        <div> {componentMetal ? <Metal /> : null}</div>
+        <div> {componentHiphop ? <Hiphop /> : null}</div>
+        <div> {componentReggae ? <Reggae /> : null}</div>
+        <div> {componentRap ? <Rap /> : null}</div>
       </div>
     </div>
   );
@@ -103,12 +116,12 @@ export default Choice;
 
 function Rock() {
   return (
-    <div className="mx-10 py-10 text-gray-50 border-gray-50 border-4 rounded-lg mt-10 flex items-center flex-col">
-      <h1 className="text-gray-300 font-bold text-xl pb-2 text-gray-800">
+    <div className="mx-10 w-96 h-96 py-10 text-gray-50 border-gray-50 border-4 rounded-lg mt-10 flex items-center flex-col">
+      <h1 className="text-gray-300 font-bold text-xl pb-2 text-gray-50">
         Rock is the best !!!
       </h1>
       <img
-        className="w-5/6 h-96 rounded-lg"
+        className="w-5/6 h-80 rounded-lg"
         src="https://c0.wallpaperflare.com/preview/285/1009/176/concery-wallpaper-festival-party.jpg"
         alt="rock-concert"
       />
@@ -118,108 +131,108 @@ function Rock() {
 }
 
 function Pop() {
-    return (
-        <div className="mx-10 py-10 bg-gray-200 rounded-lg mt-10 flex items-center flex-col">
-        <h1 className="text-gray-300 font-bold text-xl pb-2 text-gray-800">
-          Pop is the best !!!
-        </h1>
-        <img
-          className="w-5/6 h-96 rounded-lg"
-          src="https://pyxis.nymag.com/v1/imgs/3a3/b1f/2141226b8ab1ae07afe4b541ee0d2b0825-11-yic-pop-essay.rsocial.w1200.jpg"
-          alt="rock-concert"
-        />
-        <p className="">Enjoy the best Pop music of your area</p>
-      </div>
-    );
-  }
+  return (
+    <div className="mx-10 w-96 h-96 py-10 text-gray-50 border-gray-50 border-4 rounded-lg mt-10 flex items-center flex-col">
+      <h1 className="text-gray-300 font-bold text-xl pb-2 text-gray-50">
+        Pop is the best !!!
+      </h1>
+      <img
+        className="w-5/6 h-80 rounded-lg"
+        src="https://pyxis.nymag.com/v1/imgs/3a3/b1f/2141226b8ab1ae07afe4b541ee0d2b0825-11-yic-pop-essay.rsocial.w1200.jpg"
+        alt="rock-concert"
+      />
+      <p className="">Enjoy the best Pop music of your area</p>
+    </div>
+  );
+}
 
-  function Jazz() {
-    return (
-        <div className="mx-10 py-10 bg-gray-200 rounded-lg mt-10 flex items-center flex-col">
-        <h1 className="text-gray-300 font-bold text-xl pb-2 text-gray-800">
-          Jazz is the best !!!
-        </h1>
-        <img
-          className="w-5/6 h-96 rounded-lg"
-          src="http://www.jazzradio.fr/media/news/1bebe8759e23ab6a1b92e1d46e2e7f12.png"
-          alt="jazz-illus"
-        />
-        <p className="">Jazz never gets old</p>
-      </div>
-    );
-  }
-  function Electro() {
-    return (
-        <div className="mx-10 py-10 bg-gray-200 rounded-lg mt-10 flex items-center flex-col">
-        <h1 className="text-gray-300 font-bold text-xl pb-2 text-gray-800">
-          Electro is the best !!!
-        </h1>
-        <img
-          className="w-5/6 h-96 rounded-lg"
-          src="https://thetravelshots.com/wp-content/uploads/2021/04/dance-music.jpg"
-          alt="electro-illus"
-        />
-        <p className="">Keep Calm and enjoy the rythm</p>
-      </div>
-    );
-  }
-  function Metal() {
-    return (
-        <div className="mx-10 py-10 bg-gray-200 rounded-lg mt-10 flex items-center flex-col">
-        <h1 className="text-gray-300 font-bold text-xl pb-2 text-gray-800">
-          Metal is the best !!!
-        </h1>
-        <img
-          className="w-5/6 h-96 rounded-lg"
-          src="https://i2.wp.com/dynamhit.org/wp-content/uploads/2015/02/439970-metal-music-heavy-metal-fans.jpg?fit=1920%2C1200"
-          alt="rock-concert"
-        />
-        <p className="">Le rock c'est cool</p>
-      </div>
-    );
-  }
-  function Hiphop() {
-    return (
-        <div className="mx-10 py-10 bg-gray-200 rounded-lg mt-10 flex items-center flex-col">
-        <h1 className="text-gray-300 font-bold text-xl pb-2 text-gray-800">
-          Hip-Hop is the best !!!
-        </h1>
-        <img
-          className="w-5/6 h-96 rounded-lg"
-          src="https://media.timeout.com/images/101659805/image.jpg"
-          alt="rock-concert"
-        />
-        <p className="">Le rock c'est cool</p>
-      </div>
-    );
-  }
-  function Reggae() {
-    return (
-        <div className="mx-10 py-10 bg-gray-200 rounded-lg mt-10 flex items-center flex-col">
-        <h1 className="text-gray-300 font-bold text-xl pb-2 text-gray-800">
-          Reggae is the best !!!
-        </h1>
-        <img
-          className="w-5/6 h-96 rounded-lg"
-          src="https://gonzomusic.fr/files/bob-marley-03.jpg"
-          alt="rock-concert"
-        />
-        <p className="">Le rock c'est cool</p>
-      </div>
-    );
-  }
-  function Rap() {
-    return (
-        <div className="mx-10 py-10 bg-gray-200 rounded-lg mt-10 flex items-center flex-col">
-        <h1 className="text-gray-300 font-bold text-xl pb-2 text-gray-800">
-          Rap is the best !!!
-        </h1>
-        <img
-          className="w-5/6 h-96 rounded-lg"
-          src="https://i.pinimg.com/originals/6a/a4/bc/6aa4bcba8736155a31eadbd87332dc93.jpg"
-          alt="rock-concert"
-        />
-        <p className="">Le J c'est le S </p>
-      </div>
-    );
-  }
+function Jazz() {
+  return (
+    <div className="mx-10 w-96 h-96 py-10 text-gray-50 border-gray-50 border-4 rounded-lg mt-10 flex items-center flex-col">
+      <h1 className="text-gray-300 font-bold text-xl pb-2 text-gray-50">
+        Jazz is the best !!!
+      </h1>
+      <img
+        className="w-5/6 h-80 rounded-lg"
+        src="http://www.jazzradio.fr/media/news/1bebe8759e23ab6a1b92e1d46e2e7f12.png"
+        alt="jazz-illus"
+      />
+      <p className="">Jazz never gets old</p>
+    </div>
+  );
+}
+function Electro() {
+  return (
+    <div className="mx-10 w-96 h-96 py-10 text-gray-50 border-gray-50 border-4 rounded-lg mt-10 flex items-center flex-col">
+      <h1 className="text-gray-300 font-bold text-xl pb-2 text-gray-50">
+        Electro is the best !!!
+      </h1>
+      <img
+        className="w-5/6 h-80 rounded-lg"
+        src="https://thetravelshots.com/wp-content/uploads/2021/04/dance-music.jpg"
+        alt="electro-illus"
+      />
+      <p className="">Keep Calm and enjoy the rythm</p>
+    </div>
+  );
+}
+function Metal() {
+  return (
+    <div className="mx-10 w-96 h-96 py-10 text-gray-50 border-gray-50 border-4 rounded-lg mt-10 flex items-center flex-col">
+      <h1 className="text-gray-300 font-bold text-xl pb-2 text-gray-50">
+        Metal is the best !!!
+      </h1>
+      <img
+        className="w-5/6 h-80 rounded-lg"
+        src="https://i2.wp.com/dynamhit.org/wp-content/uploads/2015/02/439970-metal-music-heavy-metal-fans.jpg?fit=1920%2C1200"
+        alt="rock-concert"
+      />
+      <p className="">Le rock c'est cool</p>
+    </div>
+  );
+}
+function Hiphop() {
+  return (
+    <div className="mx-10 w-96 h-96 py-10 text-gray-50 border-gray-50 border-4 rounded-lg mt-10 flex items-center flex-col">
+      <h1 className="text-gray-300 font-bold text-xl pb-2 text-gray-50">
+        Hip-Hop is the best !!!
+      </h1>
+      <img
+        className="w-5/6 h-80 rounded-lg"
+        src="https://media.timeout.com/images/101659805/image.jpg"
+        alt="rock-concert"
+      />
+      <p className="">Le rock c'est cool</p>
+    </div>
+  );
+}
+function Reggae() {
+  return (
+    <div className="mx-10 w-96 h-96 py-10 text-gray-50 border-gray-50 border-4 rounded-lg mt-10 flex items-center flex-col">
+      <h1 className="text-gray-300 font-bold text-xl pb-2 text-gray-50">
+        Reggae is the best !!!
+      </h1>
+      <img
+        className="w-5/6 h-80 rounded-lg"
+        src="https://gonzomusic.fr/files/bob-marley-03.jpg"
+        alt="rock-concert"
+      />
+      <p className="">Le rock c'est cool</p>
+    </div>
+  );
+}
+function Rap() {
+  return (
+    <div className="mx-10 w-96 h-96 py-10 text-gray-50 border-gray-50 border-4 rounded-lg mt-10 flex items-center flex-col">
+      <h1 className="text-gray-300 font-bold text-xl pb-2 text-gray-50">
+        Rap is the best !!!
+      </h1>
+      <img
+        className="w-5/6 h-80 rounded-lg"
+        src="https://i.pinimg.com/originals/6a/a4/bc/6aa4bcba8736155a31eadbd87332dc93.jpg"
+        alt="rock-concert"
+      />
+      <p className="">Le J c'est le S </p>
+    </div>
+  );
+}
